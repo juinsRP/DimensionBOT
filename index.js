b4df727a30dfa087544372962921fd7520355aff
juinsRP <43670943+juinsRP@users.noreply.github.com>
2018-11-05 12:37:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const Discord = require('discord.js')
-const client = new Discord.Client();
+'use strict';
 
-client.login("***");
+const WebSocket = require('./lib/websocket');
+
+WebSocket.Server = require('./lib/websocket-server');
+WebSocket.Receiver = require('./lib/receiver');
+WebSocket.Sender = require('./lib/sender');
+
+module.exports = WebSocket;
